84a7f3fb5167b833a6f37b32a00826f7fbef7b4b
Brian Brunner <brian.brunner.bb@gmail.com>
2020-03-18 00:59:13 -0600

    formatting

--- public/api/save.php
+++ public/api/save.php
@@ -1,7 +1,6 @@
 <?php
 require_once('../config.php');
 
-
 try {
     $file = json_decode(file_get_contents("php://input"));
     file_put_contents($data_dir . $file->name, json_encode($file->data));
