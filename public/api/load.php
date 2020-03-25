5270490b9a17eec37b733f45ad03b8a215d651d8
Brian Brunner <brian.brunner.bb@gmail.com>
2020-03-25 00:25:26 -0600

    Fix file load api endpoint

--- public/api/load.php
+++ public/api/load.php
@@ -4,7 +4,7 @@ require_once('../config.php');
 try{
     $ret = new \stdClass;
     $ret->name = $_GET['name'];
-    $ret->data = file_get_contents($data_dir.$_GET['name']);
+    $ret->data = json_decode(file_get_contents($data_dir.$_GET['name']));
     echo json_encode($ret);
 }catch(ErrorException $exx){
     $err = new \stdClass;
